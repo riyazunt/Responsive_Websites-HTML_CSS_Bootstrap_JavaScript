e3d5e5d6aec721542ea747422c3d3b0b2fc9b414
riyazunt <35486077+riyazunt@users.noreply.github.com>
2022-05-26 06:12:18 -0400

    Add files via upload

---
+++ Microsoft_Clone_Responsive/jvascr.js
@@ -0,0 +1,8 @@
+function showOrHideDiv() {
+    var v = document.getElementById("first");
+    if (v.style.display === "none") {
+       v.style.display = "block";
+    } else {
+       v.style.display = "none";
+    }
+ }
